7c6e3a5b360d1a5a510aa9fd0a7d2568d09dc7e2
Madan <madan.patakota@gmail.com>
2023-11-10 10:50:21 +0530

    EigthClass

--- src/app/comp-c/comp-c.component.ts
+++ src/app/comp-c/comp-c.component.ts
@@ -1,7 +1,8 @@
 import { 
   Component, 
   TemplateRef, 
-  ViewChild 
+  ViewChild, 
+  ViewContainerRef
 } from '@angular/core';
 
 @Component({
@@ -12,6 +13,9 @@ import {
 export class CompCComponent {
 
 
+  CustomerAmount = "20000";
+
+
   @ViewChild('CustomerTemplate') vCustomerTemplate : TemplateRef<any>;
   displayCustomerTemplate: TemplateRef<any>;
   btnGetCustomerTemplate(){
@@ -20,7 +24,75 @@ export class CompCComponent {
 
     //console.log(this.vCustomerTemplate);
 
+
   }
+
+
+
+
+
+
+
+
+
+ 
+
+  // name : {} = "John"
+
+  @ViewChild("container" , { read : ViewContainerRef} ) vContainer : ViewContainerRef;
+
+  @ViewChild('CustomerLocationTemplate') vCustomerLocationTemplate : TemplateRef<any>;
+
+  @ViewChild('CustomerAgeTemplate')
+   vCustomerAgeTemplate : TemplateRef<any>;
+
+   isTemplatetrue = false;
+
+    // YOu have to insert | push | host |
+    // deploy the ngtemplate into the ngcontainer
+  // pen in box
+
+  GenerateTemplate(){
+
+    // I want to insert the template based on the conidtion
+    // if condition is true then i want to show 
+   //  Customer Location incase of fase i want to show Customer Age
+   if(this.isTemplatetrue){
+       // Show the CustomerLocation template
+      this.vContainer.insert(this.vCustomerLocationTemplate.createEmbeddedView(null))
+   }
+   else{
+     // Show Custoemr age template
+        this.vContainer.insert(this.vCustomerAgeTemplate.createEmbeddedView(null))
+   }
+
+
+  }
+
+
+
+  /*Target : i want to inser the ngtemplate into the containet with thedata)*/
+  
+  @ViewChild("Addresscontainer" , {read : ViewContainerRef} ) vAdressContainer : ViewContainerRef ;
+
+  @ViewChild("CustomerAddressTemplate") vCustomerAddressTemplate : TemplateRef<any>;
+
+  GenerateAdressTemplate(){
+
+     this.vAdressContainer.insert(this.vCustomerAddressTemplate.
+      createEmbeddedView(
+        { "Address" : 'HSRLayOut' }
+        ))
+
+
+  }
+
+
+
+
+
+
+
 
 
 }
